92dc9455c8e2dce61394df4a276cd2f71a76696f
Carter, Matthew A <mcarter6@caaa-21763l.gonzaga.edu>
2018-09-13 10:11:46 -0700

    Initial Commit

---
+++ Inheritance Fun/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Inheritance Fun
+//
+//  Created by Carter, Matthew A on 9/13/18.
+//  Copyright © 2018 Carter, Matthew A. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
